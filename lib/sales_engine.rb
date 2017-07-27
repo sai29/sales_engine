760685920fe7bcedc4d989dc69f896229bb13ae5
Sai Prashanth <saiprashanth93@gmail.com>
2017-07-27 11:03:24 +0530

    Associations for customers, merchants, items
    Bulk display customers, merchants, items

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -4,12 +4,45 @@ require_relative "customer/customer"
 require_relative "customer/customer_repository"
 require_relative "merchant/merchant"
 require_relative "merchant/merchant_repository"
+require "./lib/invoice/invoice"
+require_relative "invoice/invoice_repository"
+require "./lib/item/item"
+require_relative "item/item_repository"
+require "./lib/invoice_item/invoice_item"
+require_relative "invoice_item/invoice_item_repository"
 class SalesEngine
   @@customers = []
   @@merchants = []
+  @@invoices = []
+  @@items = []
+  @@invoice_items = []
+
+  def self.items
+    @@items
+  end
+
+  def self.customers
+    @@customers
+  end
+
+  def self.merchants
+    @@merchants
+  end
+
+  def self.invoices
+    @@invoices
+  end
+
+  def self.invoice_items
+    @@invoice_items
+  end
+
   def startup
     read_csv("data/customers.csv", "Customer")
     read_csv("data/merchants.csv", "Merchant")
+    read_csv("data/invoices.csv", "Invoice")
+    read_csv("data/items.csv", "Item")
+    read_csv("data/invoice_items.csv", "InvoiceItem")
   end
 
   def read_csv(file, resource)
@@ -21,6 +54,15 @@ class SalesEngine
       when "Merchant"
         merchant = Merchant.new(row.to_hash)
         @@merchants << merchant
+      when "Invoice"
+        invoice = Invoice.new(row.to_hash)
+        @@invoices << invoice
+      when "Item"
+        item = Item.new(row.to_hash)
+        @@items << item
+      when "InvoiceItem"
+        invoice_item = InvoiceItem.new(row.to_hash)
+        @@invoice_items << invoice_item
       end
     end
 
@@ -29,11 +71,14 @@ class SalesEngine
       return @@customers
     when "Merchant"
       return @@merchants
+    when "Invoice"
+      return @@invoices
+    when "Item"
+      return @@items
+    when "InvoiceItem"
+      return @@invoice_items
     end
-
   end
-
-
 
   def customer_repository
     repository = CustomerRepository.new(@@customers)
@@ -41,6 +86,7 @@ class SalesEngine
     repository.customers.each do |customer|
       p "#{customer.id}   #{customer.first_name}  #{customer.last_name}   #{customer.created_at}  #{customer.updated_at}"
     end
+    return repository
   end
 
   def merchant_repository
@@ -49,6 +95,40 @@ class SalesEngine
     repository.merchants.each do |merchant|
       p "#{merchant.id}    #{merchant.name}   #{merchant.created_at}   #{merchant.updated_at}"
     end
+    return repository
+  end
+
+  def invoice_repository
+    repository = InvoiceRepository.new(@@invoices)
+    p "Id   Name    created   Updated"
+    repository.invoices.each do |invoice|
+      p "#{invoice.id}    #{invoice.merchant_id}   #{invoice.customer_id}   #{invoice.updated_at}"
+    end
+    return repository
+  end
+
+  def item_repository
+    repository = ItemRepository.new(@@items)
+    p "Id   Name    created   Updated"
+    repository.items.each do |item|
+      p "#{item.id}    #{item.merchant_id}   #{item.unit_price}   #{item.updated_at}"
+    end
+    return repository
+  end
+
+  def invoice_item_repository
+    repository = InvoiceItemRepository.new(@@invoice_items)
+    p "Id    ItemId   InvoiceID "
+    repository.invoice_items.each do |invoice_item|
+      p "#{invoice_item.id}    #{invoice_item.item_id}   #{invoice_item.invoice_id} "
+    end
+    return repository
   end
 
 end
+
+engine = SalesEngine.new
+engine.startup
+engine.invoice_repository.invoices.first.customer
+engine.item_repository.items.first.merchant
+puts engine.invoice_item_repository
--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -3,8 +3,9 @@ require_relative '../lib/sales_engine'
 require "pry"
 
 describe SalesEngine do
+  let(:engine) { SalesEngine.new }
   describe ".read_csv" do
-    let(:engine) { SalesEngine.new }
+
 
     customer_csv_file = CSV.open("customer_csv_file", "wb") do |csv|
       csv << ["id", "first_name", "last_name", "created_at", "updated_at"]
@@ -18,4 +19,33 @@ describe SalesEngine do
         expect(customers.count).to eq(3)
     end
   end
+
+  describe ".customer_repository" do
+    it "has same count as number of customers" do
+      repository = engine.customer_repository
+      expect(repository.customers).to eq(SalesEngine.customers)
+    end
+  end
+
+  describe ".merchant_repository" do
+    it "has same count as number of merchants" do
+      repository = engine.merchant_repository
+      expect(repository.merchants).to eq(SalesEngine.merchants)
+    end
+  end
+
+  describe ".invoice_repository" do
+    it "has same count as number of invoices" do
+      repository = engine.invoice_repository
+      expect(repository.invoices).to eq(SalesEngine.invoices)
+    end
+  end
+  describe ".items_repository" do
+    it "has same count as number of items" do
+      repository = engine.item_repository
+      expect(repository.items).to eq(SalesEngine.items)
+    end
+  end
+
+
 end
